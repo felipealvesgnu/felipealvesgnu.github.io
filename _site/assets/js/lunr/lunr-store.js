9332ccdbb4d10c0cf3b61d14380223e77fbed690
Felipe A L Pereira <felipealves.gnu@gmail.com>
2019-12-28 18:32:53 -0400

    Draft

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,20 +2,25 @@ var store = [{
         "title": "Serialização e Desserialização de Objetos em Java",
         "excerpt":"Serialização Suponhamos que você tivesse um programa, um jogo com uma aventura fictícia que precisasse de mais de uma sessão para ser concluído. Conforme o jogo progride, os personagens ficammais fortes, fracos, inteligentes, etc. E coletam e usam (e perdem armas). Você não quer iniciar do zero sempre que iniciar...","categories": ["Java"],
         "tags": [],
-        "url": "https://felipealvesgnu.github.io/java/serializacao-e-desserializacao-de-objetos/",
+        "url": "http://localhost:4000/java/serializacao-e-desserializacao-de-objetos/",
         "teaser":null},{
         "title": "Install oracle client 11 (64 bits) on ubuntu(64 bits) 12",
         "excerpt":"I have recently faced some difficulties in installing the oracle client in ubuntu and have JBoss ‘see’ the client installation, since it is approved for RedHat distributions, so I would like to share with you the settings that I, with the help of some friends of the work, I was...","categories": ["Java","Linux"],
         "tags": ["JBoss","oracleclient","Ubuntu"],
-        "url": "https://felipealvesgnu.github.io/java/linux/install-oracleclient-11-64-bits-no-ubuntu64-bits-12-04/",
+        "url": "http://localhost:4000/java/linux/install-oracleclient-11-64-bits-no-ubuntu64-bits-12-04/",
         "teaser":null},{
         "title": "How to add a Server Runtime Library to a Java EE project using IntelliJ IDEA",
         "excerpt":"After I have some difficult to add server libraries to the project, because in IntelliJ is a lit bit different from Eclipse, I’ve decided to post the solution: After the project has been opened you will need go in File -&gt; Project Structure. After that you’ll see the structure of...","categories": ["Java"],
         "tags": [],
-        "url": "https://felipealvesgnu.github.io/java/how-to-add-a-server-runtime-library-to-a-java-ee-project-using-intellij-idea/",
+        "url": "http://localhost:4000/java/how-to-add-a-server-runtime-library-to-a-java-ee-project-using-intellij-idea/",
         "teaser":null},{
         "title": "IntelliJ - Application context configuration",
         "excerpt":"After spending some time trying to discover why my JSF application worked on eclipse and didn’t on IntelliJ, I found a TomCat setup configuration at Run/Debug Configurations. And it was like in that picture bellow: Without notice I was getting http status 500 error message as the pic bellow after...","categories": ["IntelliJ","Java"],
         "tags": [],
-        "url": "https://felipealvesgnu.github.io/intellij/java/intellij-application-context-configuration/",
+        "url": "http://localhost:4000/intellij/java/intellij-application-context-configuration/",
+        "teaser":null},{
+        "title": "Docker Post",
+        "excerpt":"First Draft  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/docker-post/",
         "teaser":null}]
